73543522247c8d801ba08809642974d5ad08738e
Alex van der Linden <alexvanderlinden@me.com>
2014-09-19 14:28:54 +0200

    ADC + DAC test
    - ADC en DAC merged for test purpose

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 
 #include "DAC.h"
 
-void gpioInit(void);
-
 
 int main(void)
 {
     adcInit();
-
 	dacInit();
-	dacPut(0xccc);
+
+
 
 
 	while(1)
     {
 		uint16_t adcData = adcGet();
+		dacPut(adcData);
     }
 }
