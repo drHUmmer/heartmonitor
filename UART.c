82d8b8577dc8074f91044a231e070089abde9cf3
Niels van Doorn <nielsvandoorn@outlook.com>
2014-09-19 13:12:54 +0200

    Initialization should work, NOT TESTED!

--- UART.c
+++ UART.c
@@ -1,19 +1,19 @@
 #include "UART.h"
 #include "stm32f4xx_usart.h"
+#include "stm32f4xx_gpio.h"
 
+USART_InitTypeDef USART2_InitStructure;
+USART_ClockInitTypeDef USART2_ClockStructure;
 
 void UART2_init(void) {
 
-	// UART 4 && USART 2
-
 	  GPIO_InitTypeDef GPIO_InitStructure;
-	  USART_InitTypeDef USART_InitStructure;
 
 	  /* Enable GPIO clock */
 	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	  /* Enable UART clock */
-	  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+	  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
 	  /* Connect PXx to USARTx_Tx*/
 	  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
@@ -36,17 +36,34 @@ void UART2_init(void) {
 	  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	  // UART SETTING
-	  USART_InitStructure.USART_BaudRate 			= 115200;
-	  USART_InitStructure.USART_WordLength 			= USART_WordLength_8b;
-	  USART_InitStructure.USART_StopBits 			= USART_StopBits_1;
-	  USART_InitStructure.USART_Parity 				= USART_Parity_No;
-	  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	  USART_InitStructure.USART_Mode 				=  USART_Mode_Rx | USART_Mode_Tx;
+	  USART2_InitStructure.USART_BaudRate 			= UART2_SPEED;
+	  USART2_InitStructure.USART_WordLength 			= USART_WordLength_8b;
+	  USART2_InitStructure.USART_StopBits 			= USART_StopBits_1;
+	  USART2_InitStructure.USART_Parity 				= USART_Parity_No;
+	  USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	  USART2_InitStructure.USART_Mode 				= USART_Mode_Rx | USART_Mode_Tx;
+
+	  // UART CLOCK SETTING
+	  USART2_ClockStructure.USART_CPHA				= DISABLE;
+	  USART2_ClockStructure.USART_CPOL				= DISABLE;
+	  USART2_ClockStructure.USART_Clock				= ENABLE;
+	  USART2_ClockStructure.USART_LastBit			= DISABLE;
+
+	  /* USART clock configuration */
+	  USART_ClockInit(USART2, &USART2_ClockStructure);
 
 	  /* USART configuration */
-	  USART_Init(USART2, &USART_InitStructure);
+	  USART_Init(USART2, &USART2_InitStructure);
 
 	  /* Enable USART */
 	  USART_Cmd(USART2, ENABLE);
 
+}
+
+void UART2_sendData() {
+	//USART_Word_Length ();
+}
+
+void UART2_readData() {
+	//USART_Word_Length ();
 }
--- UART.h
+++ UART.h
@@ -1,8 +1,16 @@
 #ifndef UART_H
 #define UART_H
 
+// UART 2 settings
+#define UART2_SPEED		115200
+
+// UART 4 settings
+#define UART2_SPEED		115200
+
 // Function prototypes
-void UART_init();
+void UART2_init();
+void UART2_sendData();
+void UART2_readData();
 
 
 #endif //UART_H
