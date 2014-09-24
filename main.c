76ee8c3171e07f598a3deb6ac64118863b8bdc32
Alex van der Linden <alexvanderlinden@me.com>
2014-09-24 16:24:53 +0200

    Add Timer3 en int

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include "stm32f4xx.h"
+#include "stm32f4xx_tim.h"
+#include "misc.h"
 #include "ADC.h"
 #include "DAC.h"
 
+void TIM3_IRQHandler(void){
+
+
+
+
+	//clr interrupt
+
+}
 
 int main(void)
 {
@@ -11,8 +21,6 @@ int main(void)
 	//RCC_HSICmd(ENABLE);
 	//RCC->CR &= ~(1 << 16); // disable HSE
 
-
-	while(!RCC_WaitForHSEStartUp());
 
 	/* Reset the RCC clock configuration to the default reset state ------------*/
 	/* Set HSION bit */
@@ -25,7 +33,7 @@ int main(void)
 	RCC->CR &= (uint32_t)0xFEF6FFFF;
 
 	/* Reset PLLCFGR register */
-	RCC_PLLConfig(RCC_PLLSource_HSE,10,210,2,7);
+	RCC_PLLConfig(RCC_PLLSource_HSI,10,210,2,7);
 
 	/* Reset HSEBYP bit */
 	RCC->CR &= (uint32_t)0xFFFBFFFF;
@@ -47,7 +55,7 @@ int main(void)
     RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
 
     /* Configure main PLL */
-    RCC_PLLConfig(RCC_PLLSource_HSE,10,210,2,7);
+    RCC_PLLConfig(RCC_PLLSource_HSI,10,210,2,7);
 
     /* Enable the main PLL */
     RCC->CR |= RCC_CR_PLLON;
@@ -78,6 +86,66 @@ int main(void)
 	uint32_t PCLK2_var 	= Clocks_freqs.PCLK2_Frequency;
 	uint32_t SYSCLK_var = Clocks_freqs.SYSCLK_Frequency;
 
+
+	/**********************************************
+	 * timer 2 init (32-bit timer)
+	 **********************************************/
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+
+	TIM_TimeBaseInitStruct.TIM_ClockDivision 	= TIM_CKD_DIV1;
+	TIM_TimeBaseInitStruct.TIM_CounterMode 		= TIM_CounterMode_Down;
+	TIM_TimeBaseInitStruct.TIM_Period 			= 0x0000;
+	TIM_TimeBaseInitStruct.TIM_Prescaler 		= 0x0000;
+
+	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStruct);
+
+
+	// enable nvic
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel 					= TIM3_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority 	= 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority 			= 0;
+	NVIC_InitStruct.NVIC_IRQChannelCmd 					= ENABLE;
+
+	NVIC_Init(&NVIC_InitStruct);
+
+	TIM_TypeDef TIM3_IntInitStruct;
+
+	TIM3_IntInitStruct.CR1;         /*!< TIM control register 1,              Address offset: 0x00 */
+	TIM3_IntInitStruct.CR2;         /*!< TIM control register 2,              Address offset: 0x04 */
+	TIM3_IntInitStruct.SMCR;        /*!< TIM slave mode control register,     Address offset: 0x08 */
+	TIM3_IntInitStruct.DIER;        /*!< TIM DMA/interrupt enable register,   Address offset: 0x0C */
+	TIM3_IntInitStruct.SR;          /*!< TIM status register,                 Address offset: 0x10 */
+	TIM3_IntInitStruct.EGR;         /*!< TIM event generation register,       Address offset: 0x14 */
+	TIM3_IntInitStruct.CCMR1;       /*!< TIM capture/compare mode register 1, Address offset: 0x18 */
+	TIM3_IntInitStruct.CCMR2;       /*!< TIM capture/compare mode register 2, Address offset: 0x1C */
+	TIM3_IntInitStruct.CCER;        /*!< TIM capture/compare enable register, Address offset: 0x20 */
+	TIM3_IntInitStruct.CNT;         /*!< TIM counter register,                Address offset: 0x24 */
+	TIM3_IntInitStruct.PSC;         /*!< TIM prescaler,                       Address offset: 0x28 */
+	TIM3_IntInitStruct.ARR;         /*!< TIM auto-reload register,            Address offset: 0x2C */
+	TIM3_IntInitStruct.RCR;         /*!< TIM repetition counter register,     Address offset: 0x30 */
+	TIM3_IntInitStruct.CCR1;        /*!< TIM capture/compare register 1,      Address offset: 0x34 */
+	TIM3_IntInitStruct.CCR2;        /*!< TIM capture/compare register 2,      Address offset: 0x38 */
+	TIM3_IntInitStruct.CCR3;        /*!< TIM capture/compare register 3,      Address offset: 0x3C */
+	TIM3_IntInitStruct.CCR4;        /*!< TIM capture/compare register 4,      Address offset: 0x40 */
+	TIM3_IntInitStruct.BDTR;        /*!< TIM break and dead-time register,    Address offset: 0x44 */
+	TIM3_IntInitStruct.DCR;         /*!< TIM DMA control register,            Address offset: 0x48 */
+	TIM3_IntInitStruct.DMAR;        /*!< TIM DMA address for full transfer,   Address offset: 0x4C */
+	TIM3_IntInitStruct.OR;          /*!< TIM option register,                 Address offset: 0x50 */
+
+
+
+	TIM_ITConfig(TIM3_IntInitStruct, TIM_IT_Update)
+
+
+
+
+
+	/**********************************************
+	 * gpio init
+	 **********************************************/
 
 /*
 	RCC_PLLCmd(DISABLE);
